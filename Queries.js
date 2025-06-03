e9a0032486be10d8b80361af7cd5b4bf9f44b85b
Yvonne Njoroge <ynjoroge@chlorideexide.com>
2025-06-03 16:53:32 +0300

    introduction to Mongo

--- Queries.js
+++ Queries.js
@@ -14,7 +14,7 @@ db.books.updateOne({ title: 'To Kill a Mockingbird'},{$set:{ price: 14.00,}});
 db.books.deleteOne({title: 'The Lord of the Rings'});
 
 
-//TASK 2
+//TASK 3
 // - Write a query to find books that are both in stock and published after 2010
 db.books.find({$and:[{in_stock:true},{published_year:{$gt:2010}}]});
 
@@ -36,3 +36,65 @@ db.books.find({}, { title: 1, author: 1, price: 1, _id: 0 })
   .limit(5);
 
 
+//TASK 4
+//- Create an aggregation pipeline to calculate the average price of books by genre
+db.books.aggregate([
+  {
+    $group: {
+      _id: "$genre",                    
+      averagePrice: { $avg: "$price" }  
+    }
+  },
+  {
+    $sort: { averagePrice: -1 }         
+  }
+]);
+
+//- Create an aggregation pipeline to find the author with the most books in the collection
+db.books.aggregate([
+  {
+    $group: {
+      _id: "$author",             
+      totalBooks: { $sum: 1 }     
+    }
+  },
+  {
+    $sort: { totalBooks: -1 }       
+  },
+  {
+    $limit: 1                       
+  }
+]);
+
+//- Implement a pipeline that groups books by publication decade and counts them
+db.books.aggregate([
+  {
+    $project: {
+      decade: {
+        $concat: [
+          { $toString: { $multiply: [{ $floor: { $divide: ["$year", 10] } }, 10] } },
+          "s"
+        ]
+      }
+    }
+  },
+  {
+    $group: {
+      _id: "$decade",            // Group by decade
+      count: { $sum: 1 }         // Count books in each decade
+    }
+  },
+  {
+    $sort: { _id: 1 }            // Optional: sort by decade
+  }
+]);
+
+//TASK 5
+//Create an index on the title field for faster searches
+db.books.createIndex({ title: 1 })
+
+//Create a compound index on author and published_year
+db.books.createIndex({ author: 1, published_year: -1 })
+
+//Use the explain() method to demonstrate the performance improvement with your indexes
+db.books.find({ title: "The Alchemist" }).explain("executionStats")
